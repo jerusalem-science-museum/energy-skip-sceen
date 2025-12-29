ced061deeb2cd4cbc01fe501244d71fc6a6f736e
Nathan Sebag <nathans@mada.org.il>
2025-12-29 15:37:09 +0200

    new

--- code arduino/IR_Control_Henry_Lee_Player_30_June_2017/IR_Control_Henry_Lee_Player_30_June_2017.ino
+++ code arduino/IR_Control_Henry_Lee_Player_30_June_2017/IR_Control_Henry_Lee_Player_30_June_2017.ino
@@ -18,14 +18,18 @@
 // class called "IRsend" containing only the protocols you want.
 // Now declare an instance of that sender.
 
-#define SW   (8)// sw pin 
-#define LED  (7)// led sw pin 
+#define SW_0  (8)// define input swith 0 
+#define SW_1  (9)// define input swith 1 
 #define SERIAL_BAUD_RATE  (115200)
 #define SERIAL_TIMEOUT    (1)
-const int IR_STABLE_TIME = 10;
+const int IR_STABLE_TIME = 50;
 const int PLAYER_STABLE_TIME = 1000; //ms wait after start for player stable
 const int MAX_VOLUME_LEVEL = 30; //30 plus 0 
+const uint32_t IDLE_TIME  = 300000; //ms idle time if no switch preased (visitor action) - lowr volume 
+const int DebounceTime = 100;// ms time waiting befour re-enable pushbuton check
 const int Next_Movie_Stable_Time = 500;// ms time waiting after change movie
+int sw_0_state;//switch (push buton 0) state - active low
+int sw_1_state;//switch (push buton 0) state - active low
 
 int Current_Volume_Level = 0;//assume volume level = 0 No sound
 int Optimal_Volume_Level = 20;//middle level 
@@ -33,32 +37,6 @@ uint32_t Idle_Time_Counter = 0;
 boolean Is_Silent = true;// assume silent 
  
 IRsend mySender;
-
-bool first_entry = true;
-long timer_player = 0;
-const long TIME_TO_PLAY = 10000;
-const int BOUNCE_TIME = 100;// ms time waiting befour re-enable pushbuton check
-bool check;
-
-
-
-bool PRESS_BUTTON(int IO) {
-
-  // Check if the button is pressed
-  if (digitalRead(IO) == LOW && check == LOW) {
-     //Serial.println("press :");
-     check = HIGH;         // Mark that the button is being pressed
-    delay(BOUNCE_TIME); // Apply debounce delay
-  }
-
-  // Check if the button is released
-  if (digitalRead(IO) == HIGH && check == HIGH) {
-    //Serial.println("unpress");
-    check = LOW;  // Reset the state for the next button press
-    return HIGH;  // Indicate that the button was successfully pressed and released
-  }
-  return LOW; // Return false if the button is not in the desired state
-}
 
 
 void silent(){
@@ -103,33 +81,33 @@ void set_volume_level(int Target_Volume_Level){
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
   Serial.setTimeout(SERIAL_TIMEOUT);
-  pinMode(SW, INPUT_PULLUP);// define input pins, active low 
-  pinMode(LED, OUTPUT);
+  pinMode(SW_0, INPUT_PULLUP);// define input pins, active low 
+  pinMode(SW_1, INPUT_PULLUP);
   delay(PLAYER_STABLE_TIME);// wait for player stable and start 
-  Idle_Time_Counter = millis();// reset idle counter 
   silent();
-  set_volume_level(Optimal_Volume_Level);
-  Serial.println ("init"); 
+  Idle_Time_Counter = millis();// reset idle counter 
 }
 
 void loop() {
+    if ( ((millis() - Idle_Time_Counter) >= IDLE_TIME )&&(Is_Silent == false)){
+      silent();
+      Is_Silent = true  ;   
+       }
 
-  if ( (millis() - timer_player) >= TIME_TO_PLAY){
-    if(first_entry){
-      digitalWrite(LED,HIGH);
-      first_entry = false;
-    }
-    if(PRESS_BUTTON(SW)){
-      digitalWrite(LED,LOW);
+  sw_0_state = digitalRead(SW_0);// read switch 
+  if (sw_0_state == 0) {
+    delay(DebounceTime);
+    sw_0_state = digitalRead(SW_0);// read again
+    if (sw_0_state == 0) {
+      //if (Is_Silent == true) {set_volume_level(Optimal_Volume_Level);}; // set volume level 
       mySender.send(NEC,IR_NEXT,0);//send Next IR command
-      Serial.println ("NEXT");  
+      Serial.println ("NEXT");
+      if (Is_Silent == true) {set_volume_level(Optimal_Volume_Level);}; // set volume level 
+      do{sw_0_state = digitalRead(SW_0);   
+      } while(sw_0_state == 0);// wait until switch not preased 
       Idle_Time_Counter = millis();// reset idle counter 
       delay(Next_Movie_Stable_Time); // wait 1 sec to make sure movie changed 
-
-      timer_player = millis();
-      first_entry = true;
     }
   }
-
 }
 
